d5cacbb99d6fd8463947f6d15d02209d2b1b04cb
Esmaeill <liyanboy74@gmail.com>
2022-02-07 19:25:36 +0330

    update mb-link master

--- mb-link.c
+++ mb-link.c
@@ -9,12 +9,13 @@
 
 
 uint8_t MB_LINK_Buffer[MB_LINK_Buffer_Size];
-uint8_t MB_LINK_BufferIndex=0,MB_LINK_Loop_C=0;
+uint8_t MB_LINK_BufferIndex=0,MB_LINK_Loop_C=0,MB_LINK_Func=0;
 
 void mb_link_reset_buffer(void)
 {
     MB_LINK_BufferIndex=0;
     MB_LINK_Loop_C=0;
+    MB_LINK_Func=0;
 }
 
 void mb_link_check_new_data(void)
@@ -30,30 +31,57 @@ void mb_link_check_new_data(void)
             MB_LINK_BufferIndex++;
         }else if(MB_LINK_BufferIndex==1) // Func
         {
+            MB_LINK_Func=Byte;
             MB_LINK_Buffer[MB_LINK_BufferIndex]=Byte;
             MB_LINK_BufferIndex++;
-        }else if(MB_LINK_BufferIndex==2) // Size of Data Bytes
+        }
+        else if(MB_LINK_Func==MB_Read_Coils||MB_LINK_Func==MB_Read_Discrete_Inputs||MB_LINK_Func==MB_Read_Holding_Registers||MB_LINK_Func==MB_Read_Input_Registers)
         {
-            if(Byte>MB_LINK_Max_Data_Bytes_Len)
+            if(MB_LINK_BufferIndex==2) // Size of Data Bytes
             {
-                mb_link_reset_buffer();
-                #ifdef MB_DEBUG
-                printf("[Mb-LINK:ERROR_DATA_LEN]");
-                #endif
-            }
-            else
+                if(Byte>MB_LINK_Max_Data_Bytes_Len)
+                {
+                    mb_link_reset_buffer();
+                    #ifdef MB_DEBUG
+                    printf("[Mb-LINK:ERROR_DATA_LEN]");
+                    #endif
+                }
+                else
+                {
+                    MB_LINK_Buffer[MB_LINK_BufferIndex]=Byte;
+                    MB_LINK_BufferIndex++;
+                    MB_LINK_Loop_C=Byte+MB_LINK_BufferIndex+2;
+                }
+            }else if((MB_LINK_Loop_C > MB_LINK_BufferIndex) && MB_LINK_Loop_C) // Data + CRC
             {
                 MB_LINK_Buffer[MB_LINK_BufferIndex]=Byte;
                 MB_LINK_BufferIndex++;
-                MB_LINK_Loop_C=Byte+MB_LINK_BufferIndex+2;
+                if(MB_LINK_Loop_C == MB_LINK_BufferIndex) // Data Ready
+                {
+                    if(mb_check_crc(MB_LINK_Buffer,MB_LINK_BufferIndex)==MB_CRC_OK)
+                    {
+                        #ifdef MB_DEBUG
+                        printf("[Mb-CRC:OK]");
+                        #endif
+                    }
+                    #ifdef MB_DEBUG
+                    else
+                    {
+                        printf("[Mb-CRC:ERROR]");
+                    }
+                    #endif
+                    mb_link_reset_buffer();
+                }
             }
-        }else if((MB_LINK_Loop_C > MB_LINK_BufferIndex) && MB_LINK_Loop_C) // Data + CRC
+        }
+        else if(MB_LINK_Func==MB_Write_Single_Coil||MB_LINK_Func==MB_Write_Single_Register||MB_LINK_Func==MB_Write_Multiple_Coils||MB_LINK_Func==MB_Write_Multiple_Registers)
         {
             MB_LINK_Buffer[MB_LINK_BufferIndex]=Byte;
             MB_LINK_BufferIndex++;
-            if(MB_LINK_Loop_C == MB_LINK_BufferIndex) // Data Ready
+
+            if(MB_LINK_BufferIndex>=8)
             {
-                if(mb_check_crc(MB_LINK_Buffer,MB_LINK_BufferIndex)==MB_CRC_OK)
+                if(mb_check_crc(MB_LINK_Buffer,8)==MB_CRC_OK)
                 {
                     #ifdef MB_DEBUG
                     printf("[Mb-CRC:OK]");
@@ -68,7 +96,14 @@ void mb_link_check_new_data(void)
                 mb_link_reset_buffer();
             }
         }
-        //#else //SLAVE
+        else // MB Func Not Match!
+        {
+            #ifdef MB_DEBUG
+            printf("[Mb-LINK:FUNC_NOT_SUPORTED]");
+            #endif
+            mb_link_reset_buffer();
+        }
+        #else //SLAVE
         #endif
     }
 }
--- mb-link.h
+++ mb-link.h
@@ -2,3 +2,4 @@
 #define MB_LINK_Max_Data_Bytes_Len 0x20
 
 void mb_link_check_new_data(void);
+void mb_link_reset_buffer(void);
