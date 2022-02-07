88f3ee4e12738576fa6e1d7529a7a9132d566599
Esmaeill <liyanboy74@gmail.com>
2022-02-07 17:33:45 +0330

    update mb-link

--- mb-link.c
+++ mb-link.c
@@ -2,4 +2,22 @@
 #include "mb.h"
 #include "mb-crc.h"
 
+#include "fifo.h"
+
+#define MB_LINK_Bffer_Size 128
+
+uint8_t MB_LINK_Buffer[MB_LINK_Bffer_Size];
+uint8_t MB_LINK_BufferIndex=0;
+
+void mb_link_check_new_data(void)
+{
+    uint8_t Byte;
+    static uint8_t ByteC=0;
+    if(FIFO_Read(&Byte)==FIFO_OK)
+    {
+        
+    }
+}
+
+
 
