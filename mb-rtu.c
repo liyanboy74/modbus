dad0403f84e4db77320ad576eaf6d5e18e39cb6d
Esmaeill <liyanboy74@gmail.com>
2026-07-31 20:24:02 +0330

    more clean

--- mb-rtu.c
+++ mb-rtu.c
@@ -14,8 +14,6 @@ uint8_t MB_RTU_Rx_Buffer[MB_RTU_Rx_Buffer_Size];
 uint8_t MB_RTU_Rx_Buffer_Index=0,MB_RTU_Loop_C=0,MB_RTU_Func=0;
 mb_rtu_error_e MB_RTU_Status=MB_RTU_OK;
 
-
-
 static void mb_rtu_clen(mb_rtu_clen_s *v,uint8_t f)
 {
     switch ((mb_function_e)f)
@@ -73,7 +71,7 @@ static void mb_rtu_clen(mb_rtu_clen_s *v,uint8_t f)
         break;
     }
 #if(MB_MODE==MB_MODE_MASTER)
-    if(MB_RTU_Func & 0x80)    //MB_PACKET_TYPE_ERROR
+    if(MB_RTU_Func & 0x80)
     {
         v->type=MB_RTU_TYPE_ERROR;
         v->index=5;
@@ -120,8 +118,8 @@ void mb_rtu_prepare_tx_data(mb_packet_s Packet)
     {
         MB_RTU_Tx_Buffer[i+2]=Packet.payload[i];
     }
-    mb_crc_add(MB_RTU_Tx_Buffer,i+2);  //+2=ID+FUNC
-    mb_rtu_send(MB_RTU_Tx_Buffer,i+4);//+2=CRC
+    mb_crc_add(MB_RTU_Tx_Buffer,i+2);   //+2=ID+FUNC
+    mb_rtu_send(MB_RTU_Tx_Buffer,i+4);  //+2=CRC
     return;
 }
 
@@ -139,98 +137,12 @@ void mb_rtu_check_new_data(uint8_t oneByte)
 {
     static mb_rtu_clen_s clen;
 
-#if(MB_MODE==MB_MODE_MASTER)
-
     if(!MB_RTU_Rx_Buffer_Index) //Device Address
     {
+        #if(MB_MODE==MB_MODE_MASTER)
         MB_RTU_Rx_Buffer[MB_RTU_Rx_Buffer_Index]=oneByte;
         MB_RTU_Rx_Buffer_Index++;
-    }else if(MB_RTU_Rx_Buffer_Index==1) //Func
-    {
-        MB_RTU_Func=oneByte;
-        MB_RTU_Rx_Buffer[MB_RTU_Rx_Buffer_Index]=oneByte;
-        MB_RTU_Rx_Buffer_Index++;
-        mb_rtu_clen(&clen,MB_RTU_Func);
-    }
-    else 
-    {
-        if(clen.type==MB_RTU_TYPE_VAR)
-        {
-            if(MB_RTU_Rx_Buffer_Index==clen.index)
-            {
-                if(oneByte>MB_RTU_Rx_MDBL)
-                {
-                    mb_rtu_error_handler(MB_RTU_ERROR_Data_Size);
-                    mb_rtu_reset_rx_buffer();
-                }
-                else
-                {
-                    MB_RTU_Rx_Buffer[MB_RTU_Rx_Buffer_Index]=oneByte;
-                    MB_RTU_Rx_Buffer_Index++;
-                    MB_RTU_Loop_C=(uint8_t)(oneByte+MB_RTU_Rx_Buffer_Index+2);
-                }
-            }
-            else if((MB_RTU_Loop_C > MB_RTU_Rx_Buffer_Index) && MB_RTU_Loop_C) //Data + CRC
-            {
-                MB_RTU_Rx_Buffer[MB_RTU_Rx_Buffer_Index]=oneByte;
-                MB_RTU_Rx_Buffer_Index++;
-                if(MB_RTU_Loop_C == MB_RTU_Rx_Buffer_Index) //Data Ready
-                {
-                    if(mb_crc_check(MB_RTU_Rx_Buffer,MB_RTU_Rx_Buffer_Index)==MB_CRC_OK)
-                    {
-                        //OK -> Remove CRC & Go!
-                        mb_rx_packet_handler(mb_rtu_rx_packet_split(MB_RTU_Rx_Buffer,MB_RTU_Rx_Buffer_Index-2));
-                    }
-                    else mb_rtu_error_handler(MB_RTU_ERROR_CRC);
-                    mb_rtu_reset_rx_buffer();
-                }
-            }
-        }
-        else if(clen.type==MB_RTU_TYPE_FIX)
-        {
-            MB_RTU_Rx_Buffer[MB_RTU_Rx_Buffer_Index]=oneByte;
-            MB_RTU_Rx_Buffer_Index++;
-
-            if(MB_RTU_Rx_Buffer_Index>=clen.index)
-            {
-                if(mb_crc_check(MB_RTU_Rx_Buffer,8)==MB_CRC_OK)
-                {
-                    //OK -> Remove CRC & Go!
-                    mb_rx_packet_handler(mb_rtu_rx_packet_split(MB_RTU_Rx_Buffer,MB_RTU_Rx_Buffer_Index-2));
-                }
-                else mb_rtu_error_handler(MB_RTU_ERROR_CRC);
-                mb_rtu_reset_rx_buffer();
-            }
-        }
-        else if(clen.type==MB_RTU_TYPE_ERROR)
-        {
-            MB_RTU_Rx_Buffer[MB_RTU_Rx_Buffer_Index]=oneByte;
-            MB_RTU_Rx_Buffer_Index++;
-
-            if(MB_RTU_Rx_Buffer_Index>=clen.index)
-            {
-                if(mb_crc_check(MB_RTU_Rx_Buffer,clen.index)==MB_CRC_OK)
-                {
-                    //OK -> Remove CRC & Go!
-                    mb_rx_packet_handler(mb_rtu_rx_packet_split(MB_RTU_Rx_Buffer,MB_RTU_Rx_Buffer_Index-2));
-                }
-                else mb_rtu_error_handler(MB_RTU_ERROR_CRC);
-                mb_rtu_reset_rx_buffer();
-            }
-        }
-        else
-        {
-            //MB Func Not Match!
-            mb_rtu_error_handler(MB_RTU_ERROR_FUNC);
-            mb_rtu_reset_rx_buffer();
-
-        }
-    }
-
-#elif(MB_MODE==MB_MODE_SLAVE)
-
-    if(!MB_RTU_Rx_Buffer_Index) //Device Address
-    {
+        #elif(MB_MODE==MB_MODE_SLAVE)
         if(mb.address==oneByte
         #ifdef MB_SLAVE_LISTEN_BROADCAST
         || oneByte == MB_BROADCAST_ADDRESS
@@ -241,6 +153,7 @@ void mb_rtu_check_new_data(uint8_t oneByte)
             MB_RTU_Rx_Buffer_Index++;
         }
         else mb_rtu_error_handler(MB_RTU_ERROR_Address);
+        #endif
     }
     else if(MB_RTU_Rx_Buffer_Index==1) //Func
     {
@@ -249,7 +162,7 @@ void mb_rtu_check_new_data(uint8_t oneByte)
         MB_RTU_Rx_Buffer_Index++;
         mb_rtu_clen(&clen,MB_RTU_Func);
     }
-    else
+    else 
     {
         if(clen.type==MB_RTU_TYPE_VAR)
         {
@@ -304,6 +217,24 @@ void mb_rtu_check_new_data(uint8_t oneByte)
                 mb_rtu_reset_rx_buffer();
             }
         }
+        #if(MB_MODE==MB_MODE_MASTER)
+        else if(clen.type==MB_RTU_TYPE_ERROR)
+        {
+            MB_RTU_Rx_Buffer[MB_RTU_Rx_Buffer_Index]=oneByte;
+            MB_RTU_Rx_Buffer_Index++;
+
+            if(MB_RTU_Rx_Buffer_Index>=clen.index)
+            {
+                if(mb_crc_check(MB_RTU_Rx_Buffer,clen.index)==MB_CRC_OK)
+                {
+                    //OK -> Remove CRC & Go!
+                    mb_rx_packet_handler(mb_rtu_rx_packet_split(MB_RTU_Rx_Buffer,MB_RTU_Rx_Buffer_Index-2));
+                }
+                else mb_rtu_error_handler(MB_RTU_ERROR_CRC);
+                mb_rtu_reset_rx_buffer();
+            }
+        }
+        #endif
         else
         {
             //MB Func Not Match!
@@ -311,9 +242,6 @@ void mb_rtu_check_new_data(uint8_t oneByte)
             mb_rtu_reset_rx_buffer();
         }
     }
-
-#endif
-
 }
 
 mb_packet_s mb_rtu_rx_packet_split(uint8_t *Packet_Buffer,uint8_t Len)
